eeb1240eaf6220d3a8ba6fcec27e988d91f82bdd
门那粒沙 <994266136@qq.com>
2020-02-04 18:04:36 +0800

    spring 和 mvc 分模块

---
+++ web-mvc/src/main/java/pwd/allen/config/MvcConfig.java
@@ -0,0 +1,59 @@
+package pwd.allen.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.io.IOException;
+
+/**
+ * spring MVC配置类
+ */
+@Configuration
+@ComponentScan(basePackages="pwd.allen.controller")
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setOrder(1);
+		return resolver;
+	}
+
+	/**
+	 * 配置静态资源
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	/**
+	 * 配置验证码生产工具
+	 * @return
+	 * @throws IOException
+	 */
+	@Bean
+	public DefaultKaptcha kaptcha() throws IOException {
+		DefaultKaptcha kaptcha = new DefaultKaptcha();
+		Config config = new Config(PropertiesLoaderUtils.loadProperties(new ClassPathResource("kaptcha.properties")));
+		kaptcha.setConfig(config);
+		return kaptcha;
+	}
+	
+}
---
+++ web-mvc/src/test/java/util/MockMvcTest.java
@@ -0,0 +1,88 @@
+package util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import pwd.allen.config.MainConfig;
+import pwd.allen.controller.CaptchaController;
+import pwd.allen.service.MyService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author 门那粒沙
+ * @create 2020-02-03 13:26
+ **/
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@ContextHierarchy({
+        @ContextConfiguration(classes = MainConfig.class),
+        @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml")
+})
+public class MockMvcTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Test
+    public void mockGet() throws Exception {
+        MockMvc mockMvc = null;
+        ConfigurableMockMvcBuilder mockMvcBuilder = null;
+
+        //region 构建MockMvc模拟
+        //方法一
+        mockMvcBuilder = MockMvcBuilders.webAppContextSetup(wac);
+        //方法二
+        CaptchaController captchaController = new CaptchaController();
+        //使用Mockito模拟下MyService服务依赖
+        MyService myService = Mockito.mock(MyService.class);
+        ReflectionTestUtils.setField(captchaController, "myService", myService);
+        mockMvcBuilder = MockMvcBuilders.standaloneSetup(captchaController);
+        //配置期望所有响应中的状态为200
+        mockMvcBuilder.alwaysExpect(status().isOk());
+        mockMvc = mockMvcBuilder.build();
+        //endregion
+
+        String name = "pwd";
+        mockMvc.perform(get("/test/my.do")
+                .param("name", name))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.name").value("pwd"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    /**
+     * 测试上传文件
+     * @throws Exception
+     */
+    @Test
+    public void mockUpload() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultRequest(get("/").contextPath("/pwd-web"))//设置通用的默认请求属性
+                .build();
+        String filePath = "db.properties";
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .fileUpload("/pwd-web/test/upload.do")
+                .file(new MockMultipartFile("file", filePath, "text/plain", new ClassPathResource(filePath).getInputStream())))
+                .andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+}
